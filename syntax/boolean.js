a9b75d46a50c781d23758aea273b08fadc200794
unknown <y.nia.choi@gmail.com>
2020-01-23 17:41:02 +0900

    practice8 JavaScript_boolearn comparison

---
+++ syntax/boolean.js
@@ -0,0 +1,3 @@
+console.log(true); //boolean은 이미 정해진 숫자
+console.log(false);
+true = 0;//ture=1 -> 불가능! 
--- web1_html/main.js
+++ web1_html/main.js
@@ -9,6 +9,7 @@ var app = http.createServer(function(request,response){
     console.log(queryData.id);
     if(_url == '/'){
       title = 'Welcome';
+
     }
     if(_url == '/favicon.ico'){
       return response.writeHead(404);
@@ -17,22 +18,22 @@ var app = http.createServer(function(request,response){
     fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
       var template = `
     <!doctype html>
-  <html>
-  <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="/">WEB</a></h1>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
     <ul>
       <li><a href="/?id=HTML">HTML</a></li>
       <li><a href="/?id=CSS">CSS</a></li>
       <li><a href="/?id=JavaScript">JavaScript</a></li>
     </ul>
-    <h2>${title}</h2>
-    <p>${description}</p>
-  </body>
-  </html>
+      <h2>${title}</h2>
+      <p>${description}</p>
+    </body>
+    </html>
 
     `;
     response.end(template);
