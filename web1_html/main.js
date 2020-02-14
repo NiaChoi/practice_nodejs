f12d0535dde89710f809e794bfd3ca5b127e8522
unknown <y.nia.choi@gmail.com>
2020-02-14 14:20:48 +0900

    practice 12_app filelist

--- web1_html/main.js
+++ web1_html/main.js
@@ -11,33 +11,46 @@ var app = http.createServer(function(request,response){
     
     if(pathname === '/'){
       if(queryData.id === undefined){
-       fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-        var title = 'Welcome';
-        var description = 'Hello, Node.js';
-        var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-        <ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-    
-        `;
-        
-        response.writeHead(200);
-        response.end(template);
-        });
+        fs.readdir('./data',function(error, filelist){
+          console.log(filelist);
+          var title = 'Welcome';
+          var description = 'Hello, Node.js';
+          /*
+          var list = `<ul>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ul>`;
+          */
+
+          var list = '<ul>';
+          var i = 0;
+          while(i < filelist,length){
+            list = list + `<li>${filelist[i]}</li>`;
+            i = i + 1;
+          }
+          list = list + '<ul>'
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+          
+          response.writeHead(200);
+          response.end(template);
+
+        })
+
       }else{
         fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
           var title = queryData.id;
